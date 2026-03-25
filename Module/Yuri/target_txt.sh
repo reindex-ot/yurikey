fea8a5726334fbc061daa9da5954970434df410e
Tam97123 <219667310+Tam97123@users.noreply.github.com>
2026-03-25 20:30:51 +0700

    Module: Reupload module
    Because @Yurii0307 refuse to update repo
    If I can delete repo, I would do it anyways (he never care it though)

--- Module/Yuri/target_txt.sh
+++ Module/Yuri/target_txt.sh
@@ -26,10 +26,11 @@ android
 com.android.vending?
 com.google.android.gsf?
 com.google.android.gms?
+com.google.android.ims?
 io.github.vvb2060.keyattestation?
 io.github.vvb2060.mahoshojo?
+io.github.qwq233.keyattestation?
 com.google.android.contactkeys?
-com.google.android.ims?
 com.google.android.safetycore?
 com.google.android.apps.walletnfcrel?
 com.google.android.apps.nbu.paisa.user?
@@ -41,7 +42,11 @@ me.garfieldhan.holmes?
 luna.safe.luna?
 com.zhenxi.hunter?
 com.studio.duckdetector?
-com.eltavine.duckdetector?"
+com.eltavine.duckdetector?
+com.rem01gaming.disclosure?
+wu.keyChain.test?
+com.kikyps.crackme?
+com.chunqiunativecheck?"
 for entry in $fixed_targets; do
     if ! echo "$entry" >> "$t"; then
         log_message "ERROR: Failed to write $entry to $t"
