fac44d09335ed5896dc8de655cce81c64a8ab12a
Tam97123 <219667310+Tam97123@users.noreply.github.com>
2026-04-15 16:17:25 +0700

    perf(action): Remove execute script

--- Module/action.sh
+++ Module/action.sh
@@ -13,17 +13,6 @@ do
   fi
 done
 
-# Hide Zygisk Next
-ZN="/data/adb/modules/zygisksu/bin/zygiskd"
-$ZN enforce-denylist just_umount
-$ZN memory-type anonymous
-$ZN linker builtin
-
-# Fetch new fingerprint (Play Integrity Fix [INJECT])
-PIF="/data/adb/modules/playintegrityfix"
-sh $PIF/autopif_ota.sh || true
-sh $PIF/autopif.sh
-
 if [ -f /data/adb/modules_update/Yurikey/webroot/common/device-info.sh ]; then
   sh /data/adb/modules_update/Yurikey/webroot/common/device-info.sh
 elif [ -f /data/adb/modules/yurikey/webroot/common/device-info.sh ]; then
